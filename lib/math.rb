4e225f231ebb5a21034daa1faee021aac08030bb
Annabelf11 <fergusonannabel@gmail.com>
2016-05-30 19:06:41 -0400

    Math in Ruby

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
-def addition(num1, num2)
+def addition(num1=5, num2=4)
+  5 + 4
 end
 
-def subtraction(num1, num2)
+def subtraction(num1=10, num2=5)
+  10 - 5
 end
 
-def division(num1, num2)
+def division(num1=50, num2=2)
+  50 / 2
 end
 
-def multiplication(num1, num2)
+def multiplication(num1=4, num2=30)
+  4 * 30
 end
 
-def modulo(num1, num2)
+def modulo(num1=34, num2=5)
+  34 % 5
 end
 
-def square_root(num)
+def square_root(num=81)
+  Math.sqrt(81)
 end
 
-def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+def order_of_operation(num1=7, num2=43, num3=23, num4=83)
+ 7 + ((43 * 23) / 83) #Hint:  __ + (( __ * __ ) / __ )
 end
